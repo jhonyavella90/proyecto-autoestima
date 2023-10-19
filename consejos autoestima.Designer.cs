b7cebf60842e659b5f7492b51710c46c44b91dff
jhonyavella90 <jhonyavella90@gmail.com>
2023-10-19 09:44:38 -0500

    imagenes que se mueven en autoestima

--- consejos autoestima.Designer.cs
+++ consejos autoestima.Designer.cs
@@ -28,77 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            this.boton_corazon = new System.Windows.Forms.Button();
+            this.pictureBox_corazon = new System.Windows.Forms.PictureBox();
+            this.pictureBox_mente = new System.Windows.Forms.PictureBox();
+            this.pictureBox_espiritu = new System.Windows.Forms.PictureBox();
+            this.pictureBox_cuerpo = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.timer_single_image = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_corazon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_mente)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_espiritu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_cuerpo)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // boton_corazon
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImage = global::Proyecto_1._1.Properties.Resources.recurso_fondo_2;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(113, 151);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 177);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
+            this.boton_corazon.BackColor = System.Drawing.Color.Transparent;
+            this.boton_corazon.BackgroundImage = global::Proyecto_1._1.Properties.Resources.recurso_fondo_2;
+            this.boton_corazon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.boton_corazon.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boton_corazon.FlatAppearance.BorderSize = 0;
+            this.boton_corazon.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.boton_corazon.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.boton_corazon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boton_corazon.Location = new System.Drawing.Point(170, 232);
+            this.boton_corazon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.boton_corazon.Name = "boton_corazon";
+            this.boton_corazon.Size = new System.Drawing.Size(308, 272);
+            this.boton_corazon.TabIndex = 0;
+            this.boton_corazon.UseVisualStyleBackColor = false;
+            this.boton_corazon.Click += new System.EventHandler(this.boton_corazon_Click);
             // 
-            // pictureBox1
+            // pictureBox_corazon
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::Proyecto_1._1.Properties.Resources._74;
-            this.pictureBox1.Location = new System.Drawing.Point(402, 213);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(377, 282);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox_corazon.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_corazon.Image = global::Proyecto_1._1.Properties.Resources._74;
+            this.pictureBox_corazon.Location = new System.Drawing.Point(603, 328);
+            this.pictureBox_corazon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox_corazon.Name = "pictureBox_corazon";
+            this.pictureBox_corazon.Size = new System.Drawing.Size(566, 434);
+            this.pictureBox_corazon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_corazon.TabIndex = 1;
+            this.pictureBox_corazon.TabStop = false;
+            this.pictureBox_corazon.Visible = false;
             // 
-            // pictureBox2
+            // pictureBox_mente
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = global::Proyecto_1._1.Properties.Resources._72;
-            this.pictureBox2.Location = new System.Drawing.Point(706, 213);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(378, 282);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.pictureBox_mente.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_mente.Image = global::Proyecto_1._1.Properties.Resources._72;
+            this.pictureBox_mente.Location = new System.Drawing.Point(1059, 328);
+            this.pictureBox_mente.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox_mente.Name = "pictureBox_mente";
+            this.pictureBox_mente.Size = new System.Drawing.Size(567, 434);
+            this.pictureBox_mente.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_mente.TabIndex = 2;
+            this.pictureBox_mente.TabStop = false;
+            this.pictureBox_mente.Visible = false;
             // 
-            // pictureBox3
+            // pictureBox_espiritu
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Image = global::Proyecto_1._1.Properties.Resources._71;
-            this.pictureBox3.Location = new System.Drawing.Point(755, 460);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(409, 268);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.pictureBox_espiritu.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_espiritu.Image = global::Proyecto_1._1.Properties.Resources._71;
+            this.pictureBox_espiritu.Location = new System.Drawing.Point(1132, 708);
+            this.pictureBox_espiritu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox_espiritu.Name = "pictureBox_espiritu";
+            this.pictureBox_espiritu.Size = new System.Drawing.Size(614, 412);
+            this.pictureBox_espiritu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_espiritu.TabIndex = 3;
+            this.pictureBox_espiritu.TabStop = false;
+            this.pictureBox_espiritu.Visible = false;
             // 
-            // pictureBox4
+            // pictureBox_cuerpo
             // 
-            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox4.Image = global::Proyecto_1._1.Properties.Resources._73;
-            this.pictureBox4.Location = new System.Drawing.Point(423, 435);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(378, 282);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
+            this.pictureBox_cuerpo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_cuerpo.Image = global::Proyecto_1._1.Properties.Resources._73;
+            this.pictureBox_cuerpo.Location = new System.Drawing.Point(634, 669);
+            this.pictureBox_cuerpo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox_cuerpo.Name = "pictureBox_cuerpo";
+            this.pictureBox_cuerpo.Size = new System.Drawing.Size(567, 434);
+            this.pictureBox_cuerpo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_cuerpo.TabIndex = 4;
+            this.pictureBox_cuerpo.TabStop = false;
+            this.pictureBox_cuerpo.Visible = false;
             // 
             // button2
             // 
@@ -109,9 +122,10 @@
             this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(1085, -1);
+            this.button2.Location = new System.Drawing.Point(1628, -2);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(226, 118);
+            this.button2.Size = new System.Drawing.Size(339, 182);
             this.button2.TabIndex = 5;
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -125,50 +139,58 @@
             this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(1142, 606);
+            this.button3.Location = new System.Drawing.Point(1713, 932);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(154, 89);
+            this.button3.Size = new System.Drawing.Size(231, 137);
             this.button3.TabIndex = 6;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // timer_single_image
+            // 
+            this.timer_single_image.Interval = 300;
+            this.timer_single_image.Tick += new System.EventHandler(this.timer_single_image_Tick);
+            // 
             // consejos_autoestima
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Proyecto_1._1.Properties.Resources.fondo_2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1308, 729);
+            this.ClientSize = new System.Drawing.Size(1924, 1122);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox4);
-            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Controls.Add(this.pictureBox_espiritu);
+            this.Controls.Add(this.pictureBox_corazon);
+            this.Controls.Add(this.boton_corazon);
+            this.Controls.Add(this.pictureBox_mente);
+            this.Controls.Add(this.pictureBox_cuerpo);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "consejos_autoestima";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "consejos_autoestima";
             this.Load += new System.EventHandler(this.consejos_autoestima_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_corazon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_mente)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_espiritu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_cuerpo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button boton_corazon;
+        private System.Windows.Forms.PictureBox pictureBox_corazon;
+        private System.Windows.Forms.PictureBox pictureBox_mente;
+        private System.Windows.Forms.PictureBox pictureBox_espiritu;
+        private System.Windows.Forms.PictureBox pictureBox_cuerpo;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer_single_image;
     }
 }
--- consejos autoestima.cs
+++ consejos autoestima.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,8 +18,11 @@ namespace Proyecto_1._1
         private inicio inicio = null;
         private consejos_autoestima consejos_Autoestima = null;
         private menu menu = null;
-        private Autoestima Autoestima=null;
-
+        private Autoestima Autoestima = null;
+        private PictureBox[] pictureBoxes;
+        private Image[] pictureBoxesImgOrig;
+        private int pictureBoxCount = 0;
+        private int pictureOpacity = 0;
 
         public consejos_autoestima()
         {
@@ -29,6 +35,20 @@ namespace Proyecto_1._1
             inicio = new inicio();
             menu = new menu();
             Autoestima = new Autoestima();
+            pictureBoxes = new PictureBox[4];
+            pictureBoxesImgOrig = new Image[4];
+            pictureBoxes[0] = pictureBox_corazon;
+            pictureBoxes[1] = pictureBox_mente;
+            pictureBoxes[2] = pictureBox_cuerpo;
+            pictureBoxes[3] = pictureBox_espiritu;
+            pictureBoxesImgOrig[0] = pictureBox_corazon.Image;
+            pictureBoxesImgOrig[1] = pictureBox_mente.Image;
+            pictureBoxesImgOrig[2] = pictureBox_cuerpo.Image;
+            pictureBoxesImgOrig[3] = pictureBox_espiritu.Image;
+            for (int i = 0; i < pictureBoxes.Length; i++)
+            {
+                pictureBoxes[i].Visible = false;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -39,8 +59,54 @@ namespace Proyecto_1._1
 
         private void button3_Click(object sender, EventArgs e)
         {
-            this.Hide();
+           this.Hide();
            Autoestima.Show();
+        }
+
+        private void boton_corazon_Click(object sender, EventArgs e)
+        {
+            boton_corazon.Enabled = false;
+            timer_single_image.Start();
+        }
+
+        private void timer_single_image_Tick(object sender, EventArgs e)
+        {
+            // Gradually increase the PictureBox control's Opacity property until it is fully visible.
+            pictureBoxes[pictureBoxCount].Visible = true;
+
+            SetImageOpacity(pictureBoxes[pictureBoxCount], pictureOpacity);
+
+            // If the PictureBox control is fully visible, stop the Timer control.
+            pictureOpacity += 32;
+            if (pictureOpacity >= 256)
+            {
+                timer_single_image.Stop();
+                pictureBoxCount += 1;
+                pictureOpacity = 0;
+                if (pictureBoxCount == pictureBoxes.Length) return;
+                timer_single_image.Start();
+            }
+        }
+
+        private void SetImageOpacity(PictureBox pictureBox, int opacity)
+        {
+            if (opacity > 255) opacity = 255;
+            if (pictureBox.Image == null) return;
+
+            Bitmap bmp = new Bitmap(pictureBoxesImgOrig[pictureBoxCount]);
+
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+                    if (pixel.A == 0 && pixel.R == 0 && pixel.G == 0 && pixel.G == 0) continue;
+                    Color newColor = Color.FromArgb(opacity, pixel);
+                    bmp.SetPixel(x, y, newColor);
+                }
+            }
+
+            pictureBox.Image = bmp;
         }
     }
 }
